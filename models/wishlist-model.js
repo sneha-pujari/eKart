ce883744ea3e7d4d34c79268e476306e3d21d6b0
Sneha Pujari <snehaupujari@gmail.com>
2021-05-06 18:34:47 +0530

    Delete models directory

--- models/wishlist-model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const {Schema} = mongoose;
-const wishlistItemSchema = new Schema({
-  _id: {type: Schema.Types.ObjectId, ref: "Product"},
-});
-const WishlistItem = mongoose.model("wishlistItem", wishlistItemSchema);
-
-module.exports = {WishlistItem};
